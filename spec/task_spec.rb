4c4a27b16dca72d66f2c80e44e2b9239b8ba95ce
Victor Allen <vamuigua@gmail.com>
2017-07-10 16:36:55 +0300

    Added list-id test to allow us to read the list id out

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -18,17 +18,17 @@ DB = PG.connect({:dbname => 'to_do_test'})
     
     #Test to give description
     describe("#description") do
-      it("lets you give it a description") do
-        test_task = Task.new("scrub the zebra")
-        expect(test_task.description()).to(eq("scrub the zebra"))
+      it("lets you read the description out") do
+        test_task = Task.new({:description => "learn SQL", :list_id => 1})
+        expect(test_task.description()).to(eq("learn SQL"))
       end
     end
       
     #Test method for giving all tasks when empty
     describe(".all") do
-        it("is empty at first") do
-            expect(Task.all()).to(eq([]))
-        end
+      it("is empty at first") do
+        expect(Task.all()).to(eq([]))
+      end
     end
       
     #save method that adds a new task
@@ -39,6 +39,14 @@ DB = PG.connect({:dbname => 'to_do_test'})
       expect(Task.all()).to(eq([test_task]))
     end
   end
+    
+    #mathod to allow you to read the list ID out
+    describe("#list_id") do
+      it("lets you read the list ID out") do
+        test_task = Task.new({:description => "learn SQL", :list_id => 1})
+        expect(test_task.list_id()).to(eq(1))
+      end
+    end
 
     #Test to remove tasks
     describe(".clear") do
@@ -51,10 +59,10 @@ DB = PG.connect({:dbname => 'to_do_test'})
 
     #to make two objects with the same properties are treated the same in our specs
     describe("#==") do
-      it("is the same task if it has the same description") do
-        task1 = Task.new({:description => "learn SQL"})
-        task2 = Task.new({:description => "learn SQL"})
+      it("is the same task if it has the same description and list ID") do
+        task1 = Task.new({:description => "learn SQL", :list_id => 1})
+        task2 = Task.new({:description => "learn SQL", :list_id => 1})
         expect(task1).to(eq(task2))
       end
     end
-end
+  end
