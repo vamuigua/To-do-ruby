9a2da2e63efe3a13889d8789e99c04c28c07e96b
Victor Allen <vamuigua@gmail.com>
2017-07-10 15:52:11 +0300

    Required postgres to configure the DB with the tests

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,5 +1,14 @@
 require('rspec')
-  require('task')
+require("pg")
+require('task')
+
+DB = PG.connect({:dbname => 'to_do_test'})
+
+  RSpec.configure do |config|
+    config.after(:each) do
+      DB.exec("DELETE FROM tasks *;")
+    end
+  end
 
   #To clear things out before each spec run
   describe(Task) do
