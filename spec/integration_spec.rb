9982e1faa1264320f7511a9f2be711460ff7bc8b
Victor Allen <vamuigua@gmail.com>
2017-07-11 11:03:43 +0300

    Added intergration test to see task details for a single list

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -28,7 +28,7 @@ require('capybara/rspec')
   #intergration test to see task details for a single list
   describe('seeing details for a single list', {:type => :feature}) do
     it('allows a user to click a list to see the tasks and details for it') do
-      test_list = List.new({:name => 'School stuff'})
+      test_list = List.new({:name => 'School stuff',:id=>nil})
       test_list.save()
       test_task = Task.new({:description => "learn SQL", :list_id => test_list.id()})
       test_task.save()
