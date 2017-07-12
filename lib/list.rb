2fd0c63da905458a2696211130265558f2d0e36d
Victor Allen <vamuigua@gmail.com>
2017-07-12 12:08:08 +0300

    Added method to update list in DB

--- lib/list.rb
+++ lib/list.rb
@@ -47,4 +47,10 @@ class List
       end
       list_tasks
     end
+
+    define_method(:update) do |attributes|
+      @name = attributes.fetch(:name)
+      @id = self.id()
+      DB.exec("UPDATE lists SET name = '#{@name}' WHERE id = #{@id};")
+    end
   end
