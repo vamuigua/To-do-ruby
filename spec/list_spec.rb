964ab092ebde478e06d2ad305045960cb27a8ab7
Victor Allen <vamuigua@gmail.com>
2017-07-12 12:02:42 +0300

    Added test to update my list in the DB

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -10,14 +10,14 @@ require("spec_helper")
 
     describe("#name") do
       it("tells you its name") do
-        list = List.new({:name => "moringaschool stuff", :id => nil})
+        list = List.new({:name => "moringaschool stuff", :id => 1})
         expect(list.name()).to(eq("moringaschool stuff"))
       end
     end
 
     describe("#id") do
       it("sets its ID when you save it") do
-        list = List.new({:name => "moringaschool stuff", :id => nil})
+        list = List.new({:name => "moringaschool stuff", :id => 1})
         list.save()
         expect(list.id()).to(be_an_instance_of(Fixnum))
       end
@@ -25,7 +25,7 @@ require("spec_helper")
 
     describe("#save") do
       it("lets you save lists to the database") do
-        list = List.new({:name => "moringaschool stuff", :id => nil})
+        list = List.new({:name => "moringaschool stuff", :id => 1})
         list.save()
         expect(List.all()).to(eq([list]))
       end
@@ -33,17 +33,17 @@ require("spec_helper")
 
     describe("#==") do
       it("is the same list if it has the same name") do
-        list1 = List.new({:name => "moringaschool stuff", :id => nil})
-        list2 = List.new({:name => "moringaschool stuff", :id => nil})
+        list1 = List.new({:name => "moringaschool stuff", :id => 1})
+        list2 = List.new({:name => "moringaschool stuff", :id => 1})
         expect(list1).to(eq(list2))
       end
     end
 
     describe(".find") do
       it("returns a list by its ID") do
-        test_list = List.new({:name => "Moringaschool stuff", :id => nil})
+        test_list = List.new({:name => "Moringaschool stuff", :id => 1})
         test_list.save()
-        test_list2 = List.new({:name => "Home stuff", :id => nil})
+        test_list2 = List.new({:name => "Home stuff", :id => 1})
         test_list2.save()
         expect(List.find(test_list2.id())).to(eq(test_list2))
       end
@@ -51,13 +51,22 @@ require("spec_helper")
 
     describe("#tasks") do
       it("returns an array of tasks for that list") do
-        test_list = List.new({:name => "Moringaschool stuff", :id => nil})
+        test_list = List.new({:name => "Moringaschool stuff", :id => 1})
         test_list.save()
         test_task = Task.new({:description => "Learn SQL", :list_id => test_list.id()})
         test_task.save()
         test_task2 = Task.new({:description => "Review Ruby", :list_id => test_list.id()})
         test_task2.save()
         expect(test_list.tasks()).to(eq([test_task, test_task2]))
+      end
+    end
+
+    describe("#update") do
+      it("lets you update lists in the database") do
+        list = List.new({:name => "Moringa School stuff", :id => nil})
+        list.save()
+        list.update({:name => "Homework stuff"})
+        expect(list.name()).to(eq("Homework stuff"))
       end
     end
   end
